d1f2ef9c893eb83ec96a1b7663b713757c6b0d41
Yevhenii Holubovych <102880184+Riksev@users.noreply.github.com>
2024-06-06 20:35:05 +0300

    Added interfaces and a little bit of renaming

---
+++ shopAdministration_new.js
@@ -0,0 +1,121 @@
+'use strict';
+
+/* SOLID::
+ * SRP: single responsibility principle (only one reason to change)
+ * OCP: open-closed principle 
+ * LSP: Liskov substitution principle
+ * ISP: interface segregation principle
+ * DIP: dependency inversion principle
+ */
+
+class ILogger {
+	log(message) {}
+	printLog() {}
+	clearLog() {}
+}
+
+class IFileManager {
+	save(data) {}
+	load() {}
+}
+
+class UserManager {
+	constructor() {
+			this.users = new Array();
+			this.logger = new Logger();
+			this.fileManager = new FileManager();
+	}
+
+	addUser(name, age) {
+			let user = new User(name, age);
+			this.users.push(user);
+			this.logger.log("User added: " + name);
+			this.fileManager.save(this.users);
+	}
+
+	removeUser(name) {
+			this.users = this.users.filter(user => user.name !== name);
+			this.logger.log("User removed: " + name);
+			this.fileManager.save(this.users);
+	}
+
+	printUsers() {
+			const users = new Array();
+			this.users.forEach(user => {
+				console.log(user.getInfo());
+				users.push(user.getInfo());
+			});
+			this.logger.log("Printed user list");
+			return users.join('\n');
+	}
+}
+
+class User {
+	constructor(name, age) {
+			this.name = name;
+			this.age = age;
+			this.logger = new Logger();
+	}
+
+	getInfo() {
+			this.logger.log("Getting info for user: " + this.name);
+			return this.name + " (" + this.age + " years old)";
+	}
+}
+
+class Logger extends ILogger {
+	constructor() {
+			super();
+			this.logMessages = new Array();
+	}
+
+	log(message) {
+			let timestamp = new Date().toISOString();
+			let logMessage = timestamp + " - " + message;
+			this.logMessages.push(logMessage);
+			console.log(logMessage);
+	}
+
+	printLog() {
+			const startMessage = "Log messages:";
+			const messages = new Array(["Log messages:"]);
+			console.log(startMessage);
+			this.logMessages.forEach(message => {
+				console.log(message);
+				messages.push(message);
+			});
+			return messages.join('\n');
+	}
+
+	clearLog() {
+			this.logMessages = new Array();
+			this.log("Log cleared");
+			return true;
+	}
+}
+
+class FileManager extends IFileManager {
+	save(data) {
+			let fileData = "Users: \n";
+			data.forEach(user => {
+					fileData += user.getInfo() + "\n";
+			});
+			console.log("Saving to file: \n" + fileData); // Simulate file saving
+	}
+
+	load(data, userManager) {
+			let rows = data.split('\n');
+			rows.forEach(line => {
+					if (line.startsWith("Users:")) return;
+					const [name, age] = line.replace(" years old", "").split(" (");
+					if (name && age) {
+							userManager.addUser(name, parseInt(age));
+					}
+			});
+	}
+}
+
+module.exports = {
+	UserManager
+}
+
