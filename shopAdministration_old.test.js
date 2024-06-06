7ad109e012c2f66870b1e4ad8b15db6ee1e79a0f
Yevhenii Holubovych <102880184+Riksev@users.noreply.github.com>
2024-06-06 21:24:08 +0300

    Small fixes

--- shopAdministration_old.test.js
+++ shopAdministration_old.test.js
@@ -1,59 +1,62 @@
-const { UserManager } = require('./shopAdministration_old')
+const { UserManager } = require('./shopAdministration_old');
 
 describe(
-	"Check shop administration functionality",
-	() => {
-		const testCases = [
-			{
-				function: () => {
-					let userManager = new UserManager();
-					userManager.addUser("Alice", 30);
-					userManager.addUser("Bob", 25);
-					return userManager.printUsers();
-				},
-				inString: "Test 'adding users'",
-				expected: "Alice (30 years old)\nBob (25 years old)"
-			},
-			{
-				function: () => {
-					let userManager = new UserManager();
-					const fileData = "Users: \nAlice (30 years old)\nBob (25 years old)\n";
-					userManager.fileManager.loadUsersFromFile(fileData, userManager);
-					userManager.removeUser("Alice");
-					return userManager.printUsers();
-				},
-				inString: "Test 'loading and removing users'",
-				expected: "Bob (25 years old)"
-			},
-			{
-				function: () => {
-					let userManager = new UserManager();
-					const fileData = "Users: \nAlice (30 years old)\nBob (25 years old)\n";
-					userManager.fileManager.loadUsersFromFile(fileData, userManager);
-					return userManager.logger.clearLog();
-				},
-				inString: "Test 'clearing log'",
-				expected: true
-			},
-			{
-				function: () => {
-					let userManager = new UserManager();
-					const fileData = "Users: \nAlice (30 years old)\nBob (25 years old)\n";
-					userManager.fileManager.loadUsersFromFile(fileData, userManager);
-					return userManager.logger.printLog().split('\n').length;
-				},
-				inString: "Test 'check log info'",
-				expected: 3
-			}
-		]
-		testCases.forEach(test => {
-			it(
-				`'${test.inString}', expect '${test.expected}'`,
-				() => {
-					const res = test.function()
-					expect(res).toBe(test.expected)
-				}
-			)
-		})
-	}
-)
+		"Check shop administration functionality",
+		() => {
+				let userManager;
+
+				const testCases = [
+						{
+								function: () => {
+										userManager.addUser("Alice", 30);
+										userManager.addUser("Bob", 25);
+										return userManager.printUsers();
+								},
+								inString: "Test 'adding users'",
+								expected: "Alice (30 years old)\nBob (25 years old)"
+						},
+						{
+								function: () => {
+										const fileData = "Users: \nAlice (30 years old)\nBob (25 years old)\n";
+										userManager.fileManager.loadUsersFromFile(fileData, userManager);
+										userManager.removeUser("Alice");
+										return userManager.printUsers();
+								},
+								inString: "Test 'loading and removing users'",
+								expected: "Bob (25 years old)"
+						},
+						{
+								function: () => {
+										const fileData = "Users: \nAlice (30 years old)\nBob (25 years old)\n";
+										userManager.fileManager.loadUsersFromFile(fileData, userManager);
+										return userManager.logger.clearLog();
+								},
+								inString: "Test 'clearing log'",
+								expected: true
+						},
+						{
+								function: () => {
+										const fileData = "Users: \nAlice (30 years old)\nBob (25 years old)\n";
+										userManager.fileManager.loadUsersFromFile(fileData, userManager);
+										return userManager.logger.printLog().split('\n').length;
+								},
+								inString: "Test 'check log info'",
+								expected: 3
+							}
+				];
+
+				beforeEach(() => {
+						userManager = new UserManager();
+				});
+
+				testCases.forEach(test => {
+						it(
+							`'${test.inString}', expect '${test.expected}'`,
+							() => {
+									const res = test.function()
+									expect(res).toBe(test.expected)
+							}
+						)
+				})
+		}
+);
